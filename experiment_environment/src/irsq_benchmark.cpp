673d68e23ca1e30145ccb8595b8cff9a8994c6f9
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2025-02-09 18:41:42 +0300

    add naive benchmark, refactor benchmark code

--- experiment_environment/src/irsq_benchmark.cpp
+++ experiment_environment/src/irsq_benchmark.cpp
@@ -4,6 +4,13 @@
 #include <vector>
 #include "config_loader.hpp"
 #include "random_data_generator.hpp"
+#include <benchmark/benchmark.h>
+#include <naive.hpp>
+#include <segment_tree.hpp>
+#include <experiment_manager_google.hpp>
+#include <experiment_manager_naive.hpp>
+#include <functional>
+#include <memory>
 
 namespace rsq {
 
@@ -11,149 +18,104 @@ class ExperimentManager {
 private:
     std::vector<int> random_sizes;
 
-public:
-    explicit ExperimentManager(std::vector<int> sizes)
-        : random_sizes(std::move(sizes)) {
+ public:
+  explicit ExperimentManager(std::vector<int> sizes)
+      : random_sizes(std::move(sizes)) {}
+
+  template <typename RSQType>
+  static void benchmark_naive_test(::benchmark::State &state) {
+    const size_t size = state.range(0);
+    for (auto _ : state) {
+      std::vector<int> data(size, 1);
+      RSQType rsq(data);
+      for (size_t i = 0; i < size; ++i) {
+        rsq.Update(i, i);
+        rsq.Query(0, size - 1);
+      }
     }
 
-    template <typename RSQType>
-    static void benchmark_naive_test(benchmark::State &state) {
-        const size_t size = state.range(0);
-        for (auto _ : state) {
-            std::vector<int> data(size, 1);
-            RSQType rsq(data);
-            for (size_t i = 0; i < size; ++i) {
-                rsq.Update(i, i);
-                rsq.Query(0, size - 1);
-            }
+  template <typename RSQType>
+  static void benchmark_random_test(::benchmark::State &state) {
+    const size_t size = state.range(0);
+    rsq::utils::RandomDataGenerator generator(size);
+
+    for (auto _ : state) {
+      std::vector<int> data = generator.GenerateRandomVector();
+      RSQType rsq(data);
+      for (size_t i = 0; i < size; ++i) {
+        rsq.Update(generator.GetRandomIndex(), generator.GetRandomInt());
+        std::size_t left = generator.GetRandomIndex();
+        std::size_t right = generator.GetRandomIndex();
+        if (left > right) std::swap(left, right);
+        rsq.Query(left, right);
+      }
+    }
+  }
+
+  template <typename RSQType>
+  static void benchmark_random_test_with_parameters(int min_element,
+                                                    int max_element,
+                                                    double update_probability,
+                                                    ::benchmark::State &state) {
+    const size_t size = state.range(0);
+
+    rsq::utils::RandomDataGenerator generator(size, min_element, max_element,
+                                              update_probability);
+    for (auto _ : state) {
+      std::vector<int> data = generator.GenerateRandomVector();
+      RSQType rsq(data);
+      for (size_t i = 0; i < 2 * size; ++i) {
+        if (generator.CheckChangeAction()) {
+          rsq.Update(generator.GetRandomIndex(), generator.GetRandomInt());
+        } else {
+          std::size_t left = generator.GetRandomIndex();
+          std::size_t right = generator.GetRandomIndex();
+          if (left > right) std::swap(left, right);
+          rsq.Query(left, right);
         }
+      }
+    }
+  }
+
+  void runExperiments() const {
+    for (int size : random_sizes) {
+      ::benchmark::RegisterBenchmark("BM_Naive_RSQNaive",
+                                   benchmark_naive_test<NaiveRSQ>)
+          ->Arg(size);
+      ::benchmark::RegisterBenchmark("BM_Naive_RSQSegmentTree",
+                                   benchmark_naive_test<SegmentTree>)
+          ->Arg(size);
+
+      ::benchmark::RegisterBenchmark("BM_Random_RSQNaive",
+                                   benchmark_random_test<NaiveRSQ>)
+          ->Arg(size);
+      ::benchmark::RegisterBenchmark("BM_Random_RSQSegmentTree",
+                                   benchmark_random_test<SegmentTree>)
+          ->Arg(size);
+      ::benchmark::RegisterBenchmark(
+          "BM_Random_Update_RSQNaive",
+          std::bind(benchmark_random_test_with_parameters<NaiveRSQ>, -100, 100,
+                    0.99, std::placeholders::_1))
+          ->Arg(size);
+      ::benchmark::RegisterBenchmark(
+          "BM_Random_Query_RSQNaive",
+          std::bind(benchmark_random_test_with_parameters<NaiveRSQ>, -100, 100,
+                    0.01, std::placeholders::_1))
+          ->Arg(size);
+      ::benchmark::RegisterBenchmark(
+          "BM_Random_Update_RSQSegmentTree",
+          std::bind(benchmark_random_test_with_parameters<SegmentTree>, -100,
+                    100, 0.99, std::placeholders::_1))
+          ->Arg(size);
+      ::benchmark::RegisterBenchmark(
+          "BM_Random_Query_RSQSegmentTree",
+          std::bind(benchmark_random_test_with_parameters<SegmentTree>, -100,
+                    100, 0.01, std::placeholders::_1))
+          ->Arg(size);
     }
 
-    // Бенчмарк, который тестирует работу структуры данных на случайных входных
-    // данных.
-    //
-    // Тест выполняется следующим образом:
-    // - Генерируется случайный вектор размера, переданного в аргументах,
-    // заполненный случайными числами.
-    // - В цикле выполняются случайные обновления и запросы сумм на случайных
-    // отрезках.
-    //
-    // Это позволяет измерить производительность алгоритма при случайных входных
-    // данных.
-    template <typename RSQType>
-    static void benchmark_random_test(benchmark::State &state) {
-        const size_t size = state.range(0);
-        rsq::utils::RandomDataGenerator generator(size);
-
-        for (auto _ : state) {
-            std::vector<int> data = generator.GenerateRandomVector();
-            RSQType rsq(data);
-            for (size_t i = 0; i < size; ++i) {
-                rsq.Update(
-                    generator.GetRandomIndex(), generator.GetRandomInt()
-                );
-                std::size_t left = generator.GetRandomIndex();
-                std::size_t right = generator.GetRandomIndex();
-                if (left > right)
-                    std::swap(left, right);
-                rsq.Query(left, right);
-            }
-        }
-    }
-
-    // Бенчмарк, который выполняет тест с параметрами для случайных данных.
-    //
-    // 1. **min_element** - минимальное значение, которое может принимать
-    // элемент в сгенерированном векторе.
-    //    - Пример: -100 (минимальное значение для элементов вектора).
-    // 2. **max_element** - максимальное значение для элементов вектора.
-    //    - Пример: 100 (максимальное значение для элементов вектора).
-    // 3. **update_probability** - вероятность того, что будет выполнено
-    // обновление (в отличие от запроса).
-    //    - Пример: 0.99 (вероятность выполнения обновления на каждой итерации
-    //    теста).
-    template <typename RSQType>
-    static void benchmark_random_test_with_parameters(
-        int min_element,
-        int max_element,
-        double update_probability,
-        benchmark::State &state
-    ) {
-        const size_t size = state.range(0);
-
-        rsq::utils::RandomDataGenerator generator(
-            size, min_element, max_element, update_probability
-        );
-        for (auto _ : state) {
-            std::vector<int> data = generator.GenerateRandomVector();
-            RSQType rsq(data);
-            for (size_t i = 0; i < 2 * size; ++i) {
-                if (generator.CheckChangeAction()) {
-                    rsq.Update(
-                        generator.GetRandomIndex(), generator.GetRandomInt()
-                    );
-                } else {
-                    std::size_t left = generator.GetRandomIndex();
-                    std::size_t right = generator.GetRandomIndex();
-                    if (left > right)
-                        std::swap(left, right);
-                    rsq.Query(left, right);
-                }
-            }
-        }
-    }
-
-    void runExperiments() const {
-        for (int size : random_sizes) {
-            benchmark::
-                RegisterBenchmark("BM_Naive_RSQNaive", benchmark_naive_test<NaiveRSQ>)
-                    ->Arg(size);
-            benchmark::
-                RegisterBenchmark("BM_Naive_RSQSegmentTree", benchmark_naive_test<SegmentTree>)
-                    ->Arg(size);
-
-            benchmark::
-                RegisterBenchmark("BM_Random_RSQNaive", benchmark_random_test<NaiveRSQ>)
-                    ->Arg(size);
-            benchmark::
-                RegisterBenchmark("BM_Random_RSQSegmentTree", benchmark_random_test<SegmentTree>)
-                    ->Arg(size);
-            benchmark::RegisterBenchmark(
-                "BM_Random_Update_RSQNaive",
-                std::bind(
-                    benchmark_random_test_with_parameters<NaiveRSQ>, -100, 100,
-                    0.99, std::placeholders::_1
-                )
-            )
-                ->Arg(size);
-            benchmark::RegisterBenchmark(
-                "BM_Random_Query_RSQNaive",
-                std::bind(
-                    benchmark_random_test_with_parameters<NaiveRSQ>, -100, 100,
-                    0.01, std::placeholders::_1
-                )
-            )
-                ->Arg(size);
-            benchmark::RegisterBenchmark(
-                "BM_Random_Update_RSQSegmentTree",
-                std::bind(
-                    benchmark_random_test_with_parameters<SegmentTree>, -100,
-                    100, 0.99, std::placeholders::_1
-                )
-            )
-                ->Arg(size);
-            benchmark::RegisterBenchmark(
-                "BM_Random_Query_RSQSegmentTree",
-                std::bind(
-                    benchmark_random_test_with_parameters<SegmentTree>, -100,
-                    100, 0.01, std::placeholders::_1
-                )
-            )
-                ->Arg(size);
-        }
-
-        benchmark::RunSpecifiedBenchmarks();
-    }
+    ::benchmark::RunSpecifiedBenchmarks();
+  }
 };
 
 }  // namespace rsq
