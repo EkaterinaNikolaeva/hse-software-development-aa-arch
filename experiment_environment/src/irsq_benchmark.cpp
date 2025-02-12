b9a0fa62ce09cb5ee7292c07ed2c01fc2f995bb2
Ekaterina Nikolaeva <yekaterina.nikolaeva2004@yandex.ru>
2025-02-12 23:16:14 +0300

    integrate parametrized tests

--- experiment_environment/src/irsq_benchmark.cpp
+++ experiment_environment/src/irsq_benchmark.cpp
@@ -9,8 +9,6 @@
 #include <experiment_manager_naive.hpp>
 #include <functional>
 #include <memory>
-#include <naive.hpp>
-#include <segment_tree.hpp>
 #include "config_loader.hpp"
 #include "random_data_generator.hpp"
 
